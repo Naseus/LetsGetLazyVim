4da65204daaf90d7914ae84c693056aa0d3d878a
Naseus <tdevries4i@gmail.com>
2023-03-07 22:40:22 -0500

    feat: first setup

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # 💤 LazyVim
 
-A starter template for [LazyVim](https://github.com/LazyVim/LazyVim).
+My Custom vim config based on [LazyVim](https://github.com/LazyVim/LazyVim).
 Refer to the [documentation](https://lazyvim.github.io/installation) to get started.
+
+## My Keybindings
+  + `jk` exits normal and terminal mode
+  + `<leader>ck` opens float diagnostic
--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,7 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.keymap.set("n", "<leader>ck", vim.diagnostic.open_float, { desc = "Float Diagnostic" })
+vim.keymap.set("i", "jk", "<c-\\><c-n>", { desc = "Enter Normal Mode" })
+vim.keymap.set("i", "jk", "<esc>", { desc = "Enter Normal Mode" })
