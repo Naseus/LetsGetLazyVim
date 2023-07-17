183ac909e19f00124bf044807770a98a0f624b39
Naseus <tdevries4i@gmail.com>
2023-07-16 23:47:05 -0400

    fix half my issues with lazyvim

--- lua/plugins/gruvbox.lua
+++ lua/plugins/gruvbox.lua
@@ -1,5 +1,13 @@
 if true then
   return {
-    { "ellisonleao/gruvbox.nvim" },
+    {
+      "ellisonleao/gruvbox.nvim",
+    },
+    {
+      "LazyVim/LazyVim",
+      opts = function(_, opts)
+        opts.colorscheme = "gruvbox"
+      end,
+    },
   }
 end
